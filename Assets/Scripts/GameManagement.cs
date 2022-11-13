caaf840a459b71b02d6ec3a921e395d6e14f8f44
invertedegg <invertedegg435@gmail.com>
2022-11-13 13:54:21 -0500

    added game manager object and game manager script

---
+++ Assets/Scripts/GameManagement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Game management values and checks for if the game is paused or a menu is open
+// Handles pause menu input like returning to menu or resuming the game
+
+public class GameManagement : MonoBehaviour
+{
+    private bool isPaused;
+    private string sceneNameMM; // initalize with name of main menu scene when scene exists
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
